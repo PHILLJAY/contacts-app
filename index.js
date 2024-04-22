4203d98034c0e09becd643459e97bf5ec980bc94
Philip Jasionowski <47534916+PHILLJAY@users.noreply.github.com>
2024-04-22 11:44:23 -0400

    Added env file to mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://philipjasionowski:<password>@contactappcluster.pwqh3ff.mongodb.net/?retryWrites=true&w=majority&appName=contactAppCluster";
+
+const password = process.env.MONGODB_PASSWORD;
+const uri = `mongodb+srv://philipjasionowski:${password}@contactappcluster.pwqh3ff.mongodb.net/?retryWrites=true&w=majority&appName=contactAppCluster`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -13,7 +15,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
